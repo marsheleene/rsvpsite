94e2f735f4929b331bea6fc58e2bf4e1ba0d4389
Matthieu Golsenne <matthieu.golsenne@gmail.com>
2024-06-18 19:13:51 +0200

    Updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export', // Outputs a Single-Page Application (SPA).
-    distDir: './dist', // Changes the build output directory to `./dist/`.
     assetPrefix: "./",
     images: {
       unoptimized: true,
     },
+    reactStrictMode: true,
   }
    
   export default nextConfig
