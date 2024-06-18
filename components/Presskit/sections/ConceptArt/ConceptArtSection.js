766d0291fa224a453ff6517b7df9f7183c1a9610
Matthieu Golsenne <matthieu.golsenne@gmail.com>
2024-06-18 21:51:27 +0200

    Updates

--- components/Presskit/sections/ConceptArt/ConceptArtSection.js
+++ components/Presskit/sections/ConceptArt/ConceptArtSection.js
@@ -9,7 +9,7 @@ import img_concept5 from '../../../../images/concept5.png';
 function ConceptArtSection(props) {
   return (
     <div class="md:columns-2">
-      <ImageItem className="column-span-all" src={img_concept5} alt="Concept art for the video game RSVP" />
+      <ImageItem full src={img_concept5} alt="Concept art for the video game RSVP" />
       <ImageItem src={img_concept1} alt="Concept art for the video game RSVP" />
       <ImageItem src={img_concept2} alt="Concept art for the video game RSVP" />
       <ImageItem src={img_concept3} alt="Concept art for the video game RSVP" />
--- components/Presskit/sections/ConceptArt/ImageItem.js
+++ components/Presskit/sections/ConceptArt/ImageItem.js
@@ -2,15 +2,19 @@ import Image from "next/image";
 
 function ImageItem(props) {
   let className = "break-inside-avoid-column block";
-  if (props.className) {
-    className += " " + props.className;
+  let sizes = "(max-width: 768px) 100vw, 50vw";
+  if (props.full) {
+    className += " column-span-all";
+    sizes = "(max-width: 768px) 66vw, 100vw";
   }
   return (
     <a href={props.src.src} target="_blank" className={className}>
       <Image 
-        className="w-full aspect-auto" 
+        className="aspect-auto" 
         src={props.src} 
+        sizes={sizes}
         alt={props.alt} 
+        quality={100}
       />
     </a>
   );
