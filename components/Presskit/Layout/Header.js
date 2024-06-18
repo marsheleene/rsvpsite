aab20538180fbe2711c9010c577c530539008ae9
Matthieu Golsenne <matthieu.golsenne@gmail.com>
2024-06-18 22:00:44 +0200

    Updates

--- components/Presskit/Layout/Header.js
+++ components/Presskit/Layout/Header.js
@@ -6,7 +6,7 @@ function Header() {
     return (
         <div class="flex justify-center items-center">
             <Image 
-                className="mx-8 mt-4" 
+                className="mx-4 mt-4 md:mx-8 md:mt-8" 
                 src={headerImg} 
                 alt="Keyart du jeu RSVP"
                 priority
