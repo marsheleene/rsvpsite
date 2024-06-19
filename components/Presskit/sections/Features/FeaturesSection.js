9b187228d8928d82cb38efebe45c17dfca33de55
Matthieu Golsenne <matthieu.golsenne@gmail.com>
2024-06-19 19:45:49 +0200

    Updates

--- components/Presskit/sections/Features/FeaturesSection.js
+++ components/Presskit/sections/Features/FeaturesSection.js
@@ -6,48 +6,55 @@ import icon_heart3 from "../../../../images/icon_heart3.png";
 import icon_heart4 from "../../../../images/icon_heart4.png";
 import icon_heart6 from "../../../../images/icon_heart6.png";
 
+function ListIcon(props) {
+    let className = "w-8 justify-self-start self-center";
+    if (props.className) {
+        className += " " + props.className;
+    }
+    return (
+        <Image 
+            src={props.src} 
+            alt={props.alt} 
+            quality={100}
+            className={className}
+        />
+    );
+}
+
 function FeaturesSection() {
     return (
         <Item title="Caractéristiques">
-            <div className="grid grid-cols-[4rem_auto] gap-y-4">
-                <Image 
+            <div className="grid grid-cols-[3rem_auto] gap-y-4">
+                <ListIcon 
                     src={icon_heart2} 
                     alt="Coeur" 
-                    quality={100}
-                    className="w-8 place-self-center"
                 />
                 <p>
                     Dans RSVP, le joueur incarne un·e wedding planner au service d’une famille. Il devra créer
                     des <b>plans de table</b> pour chacun des mariages de cette famille à travers le temps.
                 </p>
 
-                <Image 
+                <ListIcon 
                     src={icon_heart4} 
                     alt="Coeur" 
-                    quality={100}
-                    className="w-8 place-self-center"
                 />
                 <p>
                     Il s’agit d’<b>un mélange unique de jeu d’enquête et de puzzle</b>, dans lequel le joueur devra trouver
                     les indices qui lui permettront de déduire quels placements rempliront <b>les contraintes</b>.
                 </p>
 
-                <Image 
+                <ListIcon 
                     src={icon_heart3} 
                     alt="Coeur" 
-                    quality={100}
-                    className="w-8 place-self-center"
                 />
                 <p>
                     Les relations des personnages les uns envers les autres vont <b>évoluer en fonction de leur placement </b>
                     lors des mariages précédents. Le joueur observera <b>les conséquences de ses choix</b> lors des mariages suivants.
                 </p>
 
-                <Image 
+                <ListIcon 
                     src={icon_heart6} 
                     alt="Coeur" 
-                    quality={100}
-                    className="w-8 place-self-center"
                 />
                 <p>
                     <b>C’est le joueur qui se fixe ses objectifs</b> : il pourra essayer de créer des placements optimaux
