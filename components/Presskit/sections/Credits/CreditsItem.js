43295072a132e403ac3c6d524ea15939282af955
Matthieu Golsenne <matthieu.golsenne@gmail.com>
2024-06-19 16:13:06 +0200

    Updates

--- components/Presskit/sections/Credits/CreditsItem.js
+++ components/Presskit/sections/Credits/CreditsItem.js
@@ -1,6 +1,6 @@
 function CreditsItem(props) {
     return (
-        <div class="break-inside-avoid-column">
+        <div class="break-inside-avoid-column text-left">
             <div class="font-semibold">{props.name}</div>
             <div class="">{props.roles}</div>
         </div>
--- components/Presskit/sections/Factsheet/FactsheetItem.js
+++ components/Presskit/sections/Factsheet/FactsheetItem.js
@@ -1,6 +1,6 @@
 function FactsheetItem(props) {
     return (
-        <section class="factsheet-item">
+        <section class="factsheet-item mt-4 text-left">
             <h2 class="font-semibold">{props.title}</h2>
             <div>
                 {props.children}
