654d694c9d22395a77ac7f0f32e8da7b9bfc5f67
Matthieu Golsenne <matthieu.golsenne@gmail.com>
2024-06-20 18:28:07 +0200

    Updates

--- components/Presskit/sections/Credits/CreditsSection.js
+++ components/Presskit/sections/Credits/CreditsSection.js
@@ -12,14 +12,6 @@ function CreditsSection() {
                     <CreditsItem name="Zoé Bergeret" roles="Directrice artistique, designer" />
                 </li>
             </ul>
-            <p class="font-coquette text-xl text-center mt-4">
-                Crédits additionnels
-            </p>
-            <ul>
-                <li class="mt-4">
-                    <CreditsItem name="Elizabeth Maler" roles="Producer, designer" />
-                </li>
-            </ul>
         </Item>
     );
 }
