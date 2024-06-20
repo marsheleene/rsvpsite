de373bb3d5ed2bfe0118bdb1d968594b4a0406d1
Matthieu Golsenne <matthieu.golsenne@gmail.com>
2024-06-20 14:49:28 +0200

    Updates

--- app/layout.js
+++ app/layout.js
@@ -15,8 +15,10 @@ export default function RootLayout({ children }) {
                 <meta name="googlebot" content="nofollow" />
                 <link rel="canonical" href="https://rsvpgame.com" key="canonical" />
                 <meta name="thumbnail" content={keyart.src} />
-                <meta property="og:title" content="RSVP the video game presskit" />
-                <meta property="og:description" content="RSVP the video game presskit" />
+                <meta property="og:title" content="RSVP : le jeu vidéo" />
+                <meta property="og:description" content="La famille Lagrange des Bas-Moulins a besoin de vous pour organiser les plans de table  \
+                    de leurs mariages ! Les marié·e·s auront toujours des instructions précises que vous devez suivre. Pour le reste, libre  \
+                    à vous de satisfaire un maximum de convives, ou de les placer au gré de vos envies... et contempler les conséquences." />
                 <meta property="og:image" content={keyart.src} />
             </head>
             <body>
---
+++ app/sitemap.js
@@ -0,0 +1,9 @@
+export default function sitemap() {
+    return [
+      {
+        url: 'https://rsvpgame.com',
+        lastModified: new Date(),
+        priority: 1,
+      }
+    ]
+  }
