418bab621a87d42f2eec91e19b9c883ee262f048
Matthieu Golsenne <46344272+marsheleene@users.noreply.github.com>
2024-06-22 09:24:29 +0200

    Removed Google tag manager

--- app/layout.js
+++ app/layout.js
@@ -10,11 +10,6 @@ export default function RootLayout({ children }) {
     return (
         <html lang="fr">
             <head>
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WFKKNRN7');</script>
                 <meta name="robots" content="nofollow" />
                 <meta name="googlebot" content="nofollow" />
                 <link rel="canonical" href="https://rsvpgame.com" key="canonical" />
@@ -25,8 +20,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                 <meta property="og:image" content={keyart.src} />
             </head>
             <body>
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WFKKNRN7"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
                 <div id="root">{children}</div>
             </body>
         </html>
