48e430c59565663837a7a1bc02ddf16b55fa5141
Matthieu Golsenne <46344272+marsheleene@users.noreply.github.com>
2024-06-22 09:17:28 +0200

    Google tag manager

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,13 @@ export default function RootLayout({ children }) {
     return (
         <html lang="fr">
             <head>
+        <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WFKKNRN7');</script>
+<!-- End Google Tag Manager -->
                 <meta name="robots" content="nofollow" />
                 <meta name="googlebot" content="nofollow" />
                 <link rel="canonical" href="https://rsvpgame.com" key="canonical" />
