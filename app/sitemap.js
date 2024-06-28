534ed20f631f12859bd27b69f7a2ec210f1500c8
Matthieu Golsenne <matthieu.golsenne@gmail.com>
2024-06-28 02:20:00 +0200

    Updates

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,16 +1,16 @@
-import keyart from "../images/header_v1.png";
+// import keyart from "../images/header_v1.png";
 
-export default function sitemap() {
-    return [
-      {
-        url: 'https://rsvpgame.com',
-        lastModified: new Date(),
-        priority: 1,
-        images: [
-          {
-            loc: {keyart}
-          }
-        ]
-      }
-    ]
-  }
+// export default function sitemap() {
+//     return [
+//       {
+//         url: 'https://rsvpgame.com',
+//         lastModified: new Date(),
+//         priority: 1,
+//         images: [
+//           {
+//             loc: {keyart}
+//           }
+//         ]
+//       }
+//     ]
+//   }
