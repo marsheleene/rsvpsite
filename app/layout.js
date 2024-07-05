8ea4be5b98dabe67710bf9995a49f5e78e1a5069
Matthieu Golsenne <matthieu.golsenne@gmail.com>
2024-07-05 02:59:44 +0200

    Newsletter

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import keyart from "../images/header_v1.png";
+import keyartCropped from "../images/header-cropped.jpg";
 import background from "../images/background.jpg";
 
 import { Inter } from 'next/font/google'
@@ -28,14 +29,14 @@ export default function RootLayout({ children }) {
                 <meta name="robots" content="nofollow" />
                 <meta name="googlebot" content="nofollow" />
                 <link rel="canonical" href="https://rsvpgame.com" key="canonical" />
-                <meta name="thumbnail" content={keyart.src} />
+                <meta name="thumbnail" content={keyartCropped.src} />
                 <meta property="og:title" content="RSVP : le jeu vidéo" />
                 <meta property="og:description" content="RSVP est un jeu PC d'enquête et de puzzle narratif dans lequel vous incarnez un·e wedding planner. Votre spécialité : créer des plans de table parfaits !" />
-                <meta property="og:image" content={keyart.src} />
+                <meta property="og:image" content={keyartCropped.src} />
                 <meta content="summary_large_image" name="twitter:card" />
                 <meta name="twitter:title" content="SVP : le jeu vidéo" />
                 <meta name="twitter:description" content="RSVP est un jeu PC d'enquête et de puzzle narratif dans lequel vous incarnez un·e wedding planner. Votre spécialité : créer des plans de table parfaits !" />
-                <meta property="twitter:image" content={keyart.src} />
+                <meta property="twitter:image" content={keyartCropped.src} />
                 <meta name="twitter:url" content="https://rsvpgame.com" />
                 <meta property="twitter:creator" content="Team RSVP" />
             </head>
