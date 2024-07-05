457ebf5fe5b5c4d5e9b1b83f0b5c0be4bf4c5fa7
Matthieu Golsenne <matthieu.golsenne@gmail.com>
2024-07-05 02:52:13 +0200

    Newsletter

--- app/layout.js
+++ app/layout.js
@@ -32,7 +32,12 @@ export default function RootLayout({ children }) {
                 <meta property="og:title" content="RSVP : le jeu vidéo" />
                 <meta property="og:description" content="RSVP est un jeu PC d'enquête et de puzzle narratif dans lequel vous incarnez un·e wedding planner. Votre spécialité : créer des plans de table parfaits !" />
                 <meta property="og:image" content={keyart.src} />
-                <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+                <meta content="summary_large_image" name="twitter:card" />
+                <meta name="twitter:title" content="SVP : le jeu vidéo" />
+                <meta name="twitter:description" content="RSVP est un jeu PC d'enquête et de puzzle narratif dans lequel vous incarnez un·e wedding planner. Votre spécialité : créer des plans de table parfaits !" />
+                <meta property="twitter:image" content={keyart.src} />
+                <meta name="twitter:url" content="https://rsvpgame.com" />
+                <meta property="twitter:creator" content="Team RSVP" />
             </head>
             <body style={{'--image-url': `url(${background.src})`}} className={`${fontInter.variable} ${fontCoquette.variable}` + " font-inter bg-[image:var(--image-url)] text-mainColor"}>
                 <div id="root">
