5e5f2d0306735f2ec4d3387774ffdd6be2692192
Matthieu Golsenne <matthieu.golsenne@gmail.com>
2024-07-05 17:26:10 +0200

    Newsletter

--- app/newsletter/page.js
+++ app/newsletter/page.js
@@ -3,7 +3,9 @@ import NewsletterForm from "../../components/NewsletterForm/NewsletterForm"
 import newsletter_PNG from "../../images/newsletter.png";
 
 export const metadata = {
+    title: "RSVP : le jeu vidéo : la newsletter",
     openGraph: {
+        title: "RSVP : le jeu vidéo : la newsletter",
         images: [
             {
                 url: newsletter_PNG.src
